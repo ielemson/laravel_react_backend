92b33e6238362a1c690aaa1991b3e5d6bcc91f81
ielemson <ielemson@gmail.com>
2022-07-01 04:50:23 +0100

    updated landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,281 +1,100 @@
-@extends('layouts.welcomeLayout')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <title>Laravel</title>
 
-@section('content')
-<div class="w3l-banner-content">
-    <div class="container">
-      <div class="row">
-        <!-- class col-lg-6  pr-lg-5 -->
-        <div class="col-lg-10 bannerhny-info pr-lg-1">
-          <h3 class="mb-4">
-            All The Law<span class="w3sub-color"> Pratice Tools </span>You
-            Need In One Place
-          </h3>
-          <div class="separatorhny"></div>
-          <p class="pr-lg-5 mt-md-4 mt-3">
-            AAER provides you with valuable all-in-one tool for legal
-            research, practice management, contract management, and continuing
-            education. Subscribe to this exceptional legal resource
-          </p>
-          <a class="btn btn-style mt-sm-5 mt-4 mr-2" href="about.html">
-            Read More</a
-          >
-        </div>
-        <!-- <div class="col-lg-6 bannerhny-info-img mt-lg-0 mt-5 pl-lg-5">
-        <img src="/images/pic-1.png" alt="" class="img-fluid">
-      </div> -->
-      </div>
-    </div>
-    
-  </div>
-  <!--//w3l-banner-content-->
-  <!--/w3l-content-1-->
-  <div class="w3l-content-1 py-5">
-    <div class="container py-lg-5 py-md-4 py-2">
-      <div class="w3l-content-inf text-center">
-        <h6 class="title-subhny mb-2">About AAER</h6>
-        <h3 class="title-w3l mb-4">
-          All Appeal Electronic Reports
-        </h3>
-        <p class="para-sub pr-lg-5 mt-md-4 mt-3 mx-auto">
-          The ALL APPEAL ELECTRONIC REPORTS (AAER) is an offshoot of ABUJA
-          APPEAL QUATERLY REPORTS (AAQR), which was unveiled in 2019 by the
-          then President of the Court of Appeal, Hon. Justice Zainab Adamu
-          Bulkachuwa (Rtd.).
-        </p>
-        <a
-          class="btn btn-style btn-primary mt-sm-5 mt-4 mr-2"
-          href="about.html"
-        >
-          Read More</a
-        >
-      </div>
-    </div>
-  </div>
-  <!--//w3l-content-1-->
-  <!-- /w3l-content-2-->
-  <div class="w3l-content-2 py-5">
-    <div class="container py-md-5 py-2">
-      <div class="row align-items-center">
-        <div class="col-lg-6 whybox-wrap pr-lg-5">
-          <div class="title-content text-left">
-            <h6 class="title-subhny">AAER Features</h6>
-            <h3 class="title-w3l mb-sm-5 mb-4 pb-2">
-              AAER helps you with
-            </h3>
-          </div>
-          <div class="whybox-wrap-grid mb-4">
-            <div class="icon">
-              <span class="fa fa-wrench" aria-hidden="true"></span>
-            </div>
-            <div class="info">
-              <h4><a href="#url">All-in-one tool for legal research</a></h4>
-              <p class="mt-3">
-                Connect to our recent reported judgements, up-to-date Laws of
-                the Federation, precedents and other reference tools
-              </p>
-            </div>
-          </div>
-          <div class="whybox-wrap-grid mb-4">
-            <div class="icon">
-              <span class="fas fa-briefcase"></span>
-            </div>
-            <div class="info">
-              <h4><a href="#url">Contract Management </a></h4>
-              <p class="mt-3">
-                Lorem ipsum dolor sit amet consectetur ipsum elit. Lorem ipsum
-                dolor sit amet elit.
-              </p>
-            </div>
-          </div>
-          <div class="whybox-wrap-grid mb-4">
-            <div class="icon">
-              <span class="fa fa-book" aria-hidden="true"></span>
-            </div>
-            <div class="info">
-              <h4><a href="#url">Continuing Education </a></h4>
-              <p class="mt-3">
-                purchase or subscribe to articles, journals, textbooks and
-                other legal materials from some of the best legal minds and
-                institutions
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-6 whybox-wrap-img mt-lg-0 mt-5 pl-lg-5">
-          <div class="w3l-img-hr position-relative">
-            <img src="/images/pic-4.png" alt="" class="img-fluid" />
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- //w3l-content-2-->
-  <!-- /bottom-grids-->
-  <section class="w3l-bottom-grids-6 py-5" id="services">
-    <div class="container py-lg-5 py-md-4 py-2">
-      <div class="title-content text-center">
-        <h6 class="title-subhny text-center">AAER Differentiator</h6>
-        <h3 class="title-w3l mb-sm-5 mb-4 pb-sm-o pb-2 text-center">
-          AAER is different from other <br />
-          legal research platforms
-        </h3>
-      </div>
-      <div class="grids-area-hny text-center row mt-lg-4">
-        <div class="col-lg-3 col-md-6 grids-feature diff">
-          <div class="area-box icon">
-            <span class="fa fa-search colored-icon" aria-hidden="true"></span>
-            <h4>
-              <a href="#feature" class="title-head"
-                >Ultra-Fast Search Algorithm</a
-              >
-            </h4>
-            <p>
-              Vivamus a ligula quam tesque et libero ut justo, ultrices in. Ut
-              eu leo non. Duis sed et dolor amet.
-            </p>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-6 grids-feature diff">
-          <div class="area-box icon">
-            <span class="fas fa-laptop icon-red"></span>
-            <h4>
-              <a href="#feature" class="title-head"
-                >Powerful Real-Time Speech Synthesis</a
-              >
-            </h4>
-            <p>
-              Vivamus a ligula quam tesque et libero ut justo, ultrices in. Ut
-              eu leo non. Duis sed et dolor amet.
-            </p>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-6 grids-feature mt-md-0 mt-4 diff">
-          <div class="area-box icon">
-            <span class="fa fa-moon colored-icon"></span>
-            <h4>
-              <a href="#feature" class="title-head">
-                Intuitive Design with Dark Mode</a
-              >
-            </h4>
-            <p>
-              Vivamus a ligula quam tesque et libero ut justo, ultrices in. Ut
-              eu leo non. Duis sed dolor et amet.
-            </p>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-6 grids-feature mt-lg-0 mt-4 diff">
-          <div class="area-box icon">
-            <span class="fa fa-file colored-icon" aria-hidden="true"></span>
-            <h4>
-              <a href="#feature" class="title-head"
-                >Detailed Contract Precedents</a
-              >
-            </h4>
-            <p>
-              Vivamus a ligula quam tesque et libero ut justo, ultrices in. Ut
-              eu leo non. Duis sed dolor et amet.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  <!-- //bottom-grids-->
-  <!--/Content-mid-->
-  <section class="w3l-content-mid py-5">
-    <div class="container py-sm-4 py-2">
-      <div class="row">
-        <div class="col-lg-8">
-          <div class="bottom-info">
-            <div class="header-section">
-              <h6 class="title-subhny mb-2">Reach Us Now</h6>
-              <h3 class="title-w3l">
-                Connect with AAER, your all time legal companion
-              </h3>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 align-self text-lg-right">
-          <a
-            href="/register.html"
-            class="btn btn-style btn-primary mt-lg-0 mt-md-5 mt-4"
-            >Register
-          </a>
-        </div>
-      </div>
-    </div>
-  </section>
-  <!--//Content-mid-->
-  <!-- testimonials section -->
-  <section class="w3l-clients w3l-test pb-5" id="testimonials">
-    <div class="midd-w3 py-5">
-      <div class="container py-lg-5 py-md-4 pt-2 pb-lg-5">
-        <div class="title-content text-center">
-          <h6 class="title-subhny text-center">Happy Clients</h6>
-          <h3 class="title-w3l mb-sm-5 mb-4 pb-sm-o pb-2 text-center">
-            What Our Clients Say
-          </h3>
-        </div>
-        <div class="row mt-4 py-md-2 mb-md-4">
-          <div class="col-lg-6 item">
-            <div class="testimonial-content">
-              <div class="testimonial">
-                <blockquote>
-                  <i class="fas fa-quote-right"></i> Pellen tesque libero ut
-                  justo
-                </blockquote>
-                <p>
-                  Lorem ipsum dolor sit amet elit. hic odio tenetur. ante
-                  ipsum primis in faucibus orci luctus.Vivamus a ligula quam
-                  tesque et libero ut justo, ultrices in.
-                </p>
-              </div>
-              <div class="bottom-info mt-4">
-                <a class="comment-img" href="#url"
-                  ><img
-                    src="/images/team1.jpg"
-                    class="img-responsive"
-                    alt="placeholder image"
-                /></a>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-                <div class="people-info align-self">
-                  <h3>Johnson william</h3>
-                  <p class="identity">Example City</p>
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            {{-- @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
                 </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-6 item mt-lg-0 mt-5">
-            <div class="testimonial-content">
-              <div class="testimonial">
-                <blockquote>
-                  <i class="fas fa-quote-right"></i> Lorem ipsum dolor sit
-                  amet
-                </blockquote>
-                <p>
-                  Lorem ipsum dolor sit amet elit. hic odio tenetur. ante
-                  ipsum primis in faucibus orci luctus.Vivamus a ligula quam
-                  tesque et libero ut justo, ultrices in.
-                </p>
-              </div>
-              <div class="bottom-info mt-4">
-                <a class="comment-img" href="#url"
-                  ><img
-                    src="/images/team2.jpg"
-                    class="img-responsive"
-                    alt="placeholder image"
-                /></a>
+            @endif --}}
 
-                <div class="people-info align-self">
-                  <h3>Alexander sakura</h3>
-                  <p class="identity">Example City</p>
+            <div class="content">
+                <div class="title m-b-md">
+                    Laravel
                 </div>
-              </div>
+
+                <div class="links">
+                    <a href="https://laravel.com/docs">Docs</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://blog.laravel.com">Blog</a>
+                    <a href="https://nova.laravel.com">Nova</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://vapor.laravel.com">Vapor</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  </section>
-@endsection
+    </body>
+</html>
