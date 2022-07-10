137b5987096edd22fab77e92ada3f1666697c3ae
ielemson <ielemson@gmail.com>
2022-07-10 10:09:05 +0100

    updated user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('picture')->nullable();
             $table->string('password');
+            $table->integer('active')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
